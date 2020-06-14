a701d094c2a129a4b03902f9a61d0467838bd3c7
ShunyaSuzuki <anyushu2017@gmail.com>
2020-06-14 14:00:30 +0900

    update

--- header.php
+++ header.php
@@ -12,10 +12,29 @@ $wp_url = get_template_directory_uri(); ?>
 <?php endif; ?>
 </head>
 <body>
-<!-- ヘッダー -->
-<header>
-</header>
-<!-- ヘッダー終了 -->
 
-<!-- メインコンテンツ -->
+<header class="header">
+<div class="container position-relative">
+<nav class="navbar navbar-expand-md navbar-light">
+<h1 class="navbar-brand"><a href="<?php echo $home; ?>"><img src="<?php echo $wp_url; ?>/dist/images/logo.png" alt="<?php bloginfo("name"); ?>"></a></h1>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav w-100 justify-content-center">
+<li class="nav-item"><a class="nav-link text-primary" href="<?php echo $home; ?>/">home</a></li>
+<li class="nav-item"><a class="nav-link text-primary" href="<?php echo $home; ?>/about/">about</a></li>
+<li class="nav-item"><a class="nav-link text-primary" href="<?php echo $home; ?>/menu/">menu</a></li>
+<li class="nav-item"><a class="nav-link text-primary" href="<?php echo $home; ?>/blog/">blog</a></li>
+<li class="nav-item"><a class="nav-link text-primary" href="<?php echo $home; ?>/staff/">staff</a></li>
+<li class="nav-item"><a class="nav-link text-primary" href="<?php echo $home; ?>/access/">access</a></li>
+</ul>
+</div>
+</nav>
+
+<a class="header-reserve" href="https://comile.bionly.net/" target="_blank"><img src="<?php echo $wp_url; ?>/dist/images/btn_reserve.png" alt="予約する"></a>
+
+</div>
+</header>
+
 <main>
